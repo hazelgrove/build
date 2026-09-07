f7c8f5368728cc11bd0557eeb3fa31b6307b6e02
github-deploy-action <hazel-deploy@hazel.org>
2026-09-07 15:11:21 +0000

    github-deploy-action-modules-ii/5-projection-improvements

--- modules-ii/5-projection-improvements/hazel.js
+++ modules-ii/5-projection-improvements/hazel.js
@@ -32639,7 +32639,6 @@ M=j[4][4].call(null,0,0);return uz(j[4][23].call(null,0,i,M),c);case
 8:case
 10:case
 11:case
-15:case
 16:break;default:return c}return a6("prepend_ctr called with a non-normalized type: "+n(aFk,b))}var
 zP=aJ(iEm);function
 bzG(c,b){return c[6]?uy:bZU(function(a){return 1-q(Sv[31],a,c[5])},H3(n(zO[13],b))[1])}function
@@ -32673,7 +32672,6 @@ e==="number"?lA(uy,zP,d):cw4(c,b,bzG(a,e[1]),d);case
 8:case
 10:case
 11:case
-15:case
 16:break;default:return lA(uy,zP,d)}return a6("cons_from_type called with a non-normalized type: "+n(aFk,b))}function
 cw6(a,b,c){var
 d=bzE(0,a),f=c[1],e=a[1];if(typeof
@@ -32695,7 +32693,6 @@ d==="number"?lA(uy,zP,c):cw4(b,a,n(zO[18],d[1])[1],c);case
 8:case
 10:case
 11:case
-15:case
 16:break;default:return lA(uy,zP,c)}return a6("prepend_from_type called with a non-normalized type: "+n(aFk,a))}function
 WT(a,b,c){return a?[0,b,c]:c}function
 WU(a,b,c,d){return O(zO[7],a,function(a){return a?[0,[0,[0,b,c],a[1]]]:[0,[0,[0,b,c],0]]},d)}var
